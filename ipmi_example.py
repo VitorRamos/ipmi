68fd85d7591fa2decd18878103826c446299f686
vitorramos <ramos.vitor89@gmail.com>
2019-08-14 15:43:00 +0200

    refactored

--- ipmi_example.py
+++ ipmi_example.py
@@ -4,14 +4,7 @@ import os
 
 ipmi_sensor= ipmi.IPMI("http://localhost:8080", "admin", "admin")
 
-try:
-	while 1:
-		ipmi_sensor.get_data()
-#		os.system('clear')
-		ipmi_sensor.print_data()
-		time.sleep(1)
-except KeyboardInterrupt:
-	ipmi_sensor.save_data("data")
-	ipmi_sensor.clear_data()
-	print "Saving"
-	
+while 1:
+	data= ipmi_sensor.get_data()
+	print(data)
+	print()
